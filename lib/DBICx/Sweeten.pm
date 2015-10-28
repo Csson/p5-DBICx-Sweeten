867e814bed2eeac8921ed89a23426a8eafb01372
Erik Carlsson <info@code301.com>
2015-10-28 23:37:34 +0000

    Init

---
+++ lib/DBICx/Sweeten.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package DBICx::Sweeten;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use DBICx::Sweeten;
+
+=head1 DESCRIPTION
+
+DBICx::Sweeten is ...
+
+=head1 SEE ALSO
+
+=cut
